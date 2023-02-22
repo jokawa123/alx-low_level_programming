de9cee3976e984c1e7daca799f896c2a0f10b1e4
Okawa Josephine <okawajosephine@gmail.com>
2023-02-22 15:27:39 -0800

    modified 3-islower.c and 4-isalpha.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,9 +3,11 @@
 /**
  * _islower-check if character is lowercase.
  *
+ * @c:character to check if it islowercase.
+ *
  * Return: 0 or 1
  */
 int _islower(int c)
 {
-return (int) c >= 'a' && c <= 'z';
+return (int) (c >= 'a' && c <= 'z');
 }
--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,9 +3,11 @@
 /**
  * _isalpha-check if character is a letter, lowercase or uppercase.
  *
+ * @c:character to check if it islowercase.
+ *
  * Return: 0 or 1
  */
 int _isalpha(int c)
 {
-return (int)( c >= 'a' && c <= 'z' ) || ( c >= 'A' && c < 'Z' );
+return (int)((c >= 'a' && c <= 'z') || (c >= 'A' && c < 'Z'));
 }
