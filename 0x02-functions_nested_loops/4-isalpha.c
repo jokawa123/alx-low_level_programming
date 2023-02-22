3fab8c159b0b2460e846dcb21c655d52e0112004
Okawa Josephine <okawajosephine@gmail.com>
2023-02-22 15:06:04 -0800

    added 3-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isalpha-check if character is a letter, lowercase or uppercase.
+ *
+ * Return: 0 or 1
+ */
+int _isalpha(char c)
+{
+  return (int)( c >= 'a' && c <= 'z' ) || ( c >= 'A' && c < 'Z' );
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,4 +8,6 @@ extern void print_alphabet(void);
 extern void print_alphabet_x10(void);
 
 extern int _islower(int c);
+
+extern int _isalpha(int c);
 #endif
