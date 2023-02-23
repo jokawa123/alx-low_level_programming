5a79bac88b00f0ff213177947c4056eb78310aa2
Okawa Josephine <okawajosephine@gmail.com>
2023-02-22 16:25:29 -0800

    added 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit-prints the last digit of a number.
+ *
+ * @n:number to check.
+ *
+ * Return: value of the last digit
+ */
+int print_last_digit(int n)
+{
+int l = a % 10;
+return (_abs(l));
+}
