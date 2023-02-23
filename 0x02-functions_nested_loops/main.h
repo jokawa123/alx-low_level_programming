43048cb8a1ca7ccb7f1762ae2392a10ac6e5eaf5
Okawa Josephine <okawajosephine@gmail.com>
2023-02-22 16:47:19 -0800

    added 10-add.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -14,4 +14,14 @@ extern int _isalpha(int c);
 extern int print_sign(int n);
 
 extern int _abs(int);
+
+extern int print_last_digit(int);
+
+extern void jack_bauer(void);
+
+extern void times_table(void);
+
+extern int add(int, int);
+
+extern void print_to_98(int n);
 #endif
